5210853874e37637eebaadb2c2b598b2f8549b13
eduardojordane <luiz.jordane@gmail.com>
2023-11-08 08:17:16 -0300

    Subindo arquivos do projeto antigo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Router from "./components/Router/Router";
+import Header from './components/Header/Header'
+
+//Linha de teste
+
+function App() {
+  return (
+    <div className="App" >
+      <Header />
+      <Router />
+    </div>
+  );
+}
+
+export default App;
